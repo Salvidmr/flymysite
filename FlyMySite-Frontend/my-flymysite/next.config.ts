1fe4378b423edd314a982cd0aa2e4e732bf6c7ef
Salvidmr <165647969+Salvidmr@users.noreply.github.com>
2025-07-02 22:43:23 +0200

    des

--- FlyMySite-Frontend/my-flymysite/next.config.ts
+++ FlyMySite-Frontend/my-flymysite/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.modules.push(__dirname + '/src');
+    return config;
+  }
 };
 
 export default nextConfig;
